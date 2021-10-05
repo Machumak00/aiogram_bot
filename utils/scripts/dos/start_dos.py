21c557918290e7c1ee2da4f1db57cc3f0df19141
machumak00 <misha7877@gmail.com>
2021-10-06 01:34:32 +0500

    Added 1 second ping

--- utils/scripts/dos/start_dos.py
+++ utils/scripts/dos/start_dos.py
@@ -2,9 +2,9 @@ import asyncio
 import logging
 import os.path
 
-from aiogram.dispatcher.handler import current_handler
 from aiogram.dispatcher.storage import FSMContextProxy
 from aiogram.types import Message
+
 from loader import dp
 
 
@@ -16,7 +16,6 @@ async def get_ping(message: Message, dos_data: FSMContextProxy):
     current_path = os.path.dirname(os.path.abspath(__file__))
     msg = await message.answer(f"Текущее состояние пакетов по данному IP-адресу:")
     while await dp.current_state().get_state() != 'DosState:stopped_script':
-        logging.info(await dp.current_state().get_state())
         cmd = f"sh '{current_path}/ping.sh' {dos_data['ip']}"
         proc = await asyncio.create_subprocess_shell(
             cmd=cmd,
@@ -30,7 +29,6 @@ async def get_ping(message: Message, dos_data: FSMContextProxy):
 async def start_dos(dos_data: FSMContextProxy):
     current_path = os.path.dirname(os.path.abspath(__file__))
     cmd = f"sh '{current_path}/dos.sh' {current_path}/dos_lib {dos_data['ip']} {dos_data['port']} {dos_data['size']}"
-    logging.info(100)
     proc = await asyncio.create_subprocess_shell(
         cmd=cmd,
         stdout=asyncio.subprocess.PIPE,
